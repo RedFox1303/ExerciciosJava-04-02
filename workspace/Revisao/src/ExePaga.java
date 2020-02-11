278eab315c8d6058c66dcb6003fb98cd22306168
RedFox1303 <56698813+RedFox1303@users.noreply.github.com>
2020-02-11 13:52:09 -0300

    Update ExePaga.java

--- workspace/Revisao/src/ExePaga.java
+++ workspace/Revisao/src/ExePaga.java
@@ -4,16 +4,14 @@ public class ExePaga {
 	public static void main(String[] args) {
 		Scanner in=new Scanner(System.in);
 		
-		double salB, salL, salF;
+		double salB, salL;
 		
-		System.out.println("Digite teu sal·rio bruto");
+		System.out.println("Digite teu sal√°rio bruto");
 		salB=in.nextDouble();
 		
-		salL = salB + (salB*0.1);
-		System.out.println(salL);
-		salF = salB - (salB*0.2);
+		salL = salB + (salB*0.1) - (salB*0.2);
 		
-		System.out.println("seu salario liquido, com os descontos fica: "+salF);
+		System.out.println("seu salario liquido, com os descontos fica: "+salL);
 	}
 
 }
